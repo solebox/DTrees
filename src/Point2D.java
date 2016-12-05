11c0c402151db147a99708a132bdd4557fb9e8df
solebox <5013box@gmail.com>
2016-12-05 15:09:43 +0200

    added the interfaces

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,5 +1,11 @@
-/**
- * Created by sole on 12/5/16.
- */
-public class Point2D {
+public class Point2D implements Comparable<Point2D> {
+    public Point2D(double x, double y)              // construct the point (x, y)
+    public  double x()                              // x-coordinate
+    public  double y()                              // y-coordinate
+    public  double distanceTo(Point2D that)         // Euclidean distance between two points
+    public  double distanceSquaredTo(Point2D that)  // square of Euclidean distance between two points
+    public     int compareTo(Point2D that)          // for use in an ordered symbol table
+    public boolean equals(Object that)              // does this point equal that object?
+    public    void draw()                           // draw to standard draw
+    public  String toString()                       // string representation
 }
